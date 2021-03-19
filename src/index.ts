7ebc5c5e8d6b59dbfb1e251994c32c433fb566b8
Crgio <randaledomman1@gmail.com>
2021-03-19 06:25:45 -0300

    socket.io connected

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-import express from 'express';
-
+const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const PORT:number = Number(process.env.PORT) || 4000;
+const io = require('socket.io')(server);
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).send({ message: 'server is working' });
+});
+
+io.on('connect', (socket) => {
+  console.log('a connection');
+
+  socket.on('disconnect', () => {
+    console.log('a disconnection');
+  });
 });
 
 server.listen(PORT, (err) => {
